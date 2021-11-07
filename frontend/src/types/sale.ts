070579d67eaf1d1409f2edbf3369a44ee15154ed
Márcio <marcioalvesbj@gmail.com>
2021-11-07 13:41:16 -0300

    Data table integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,27 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number
+    visited: number
+    deals: number
+    amount: number
+    date: string
+    seller: Seller
+}
+
+export type SalePage = {
+    content?: Sale[]
+
+    first: boolean
+    last: boolean
+    totalElements: number
+    totalPages: number
+    size?: number
+    number: number
+    numberOfElements?: number
+    empty?: boolean
+}
+
 export type SaleSum = {
     sellerName: string,
     sum: number
--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,5 +1,14 @@
+import {format} from 'date-fns'
+
 //função para arredondar números
 export const Round = (value: number, precision: number) => {
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier;
+}
+
+//fortatação data
+export const formatLocalDate = (date: string, pattern: string) => {
+    const dt = new Date(date);
+    const dtDateOnly = new Date(dt.valueOf() + dt.getTimezoneOffset() * 60 * 1000);
+    return format(dtDateOnly, pattern);
 }
